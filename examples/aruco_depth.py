4e667543965ae3ce303d09fb92af01d56fdb7ade
Gustav <gtc@vitalthings.com>
2025-04-25 15:15:18 +0200

    track emblobot

--- examples/aruco_depth.py
+++ examples/aruco_depth.py
@@ -1,9 +1,12 @@
+#%%
 import cv2
 import pykinect_azure as pykinect
 import numpy as np
-from pykinect_azure import K4A_CALIBRATION_TYPE_COLOR, K4A_CALIBRATION_TYPE_DEPTH, k4a_float2_t
+from pykinect_azure import K4A_CALIBRATION_TYPE_COLOR, K4A_CALIBRATION_TYPE_DEPTH, k4a_float2_t, k4a_float3_t
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
+# if True:
 
 	# Initialize the library, if the library is not found, add the library path as argument
 	pykinect.initialize_libraries()
@@ -48,19 +51,67 @@ if __name__ == "__main__":
 		corners, ids, rejected = detector.detectMarkers(gray)
 		# ret, depth_image = capture.get_depth_image()
 		if ids is not None:
-			transformed_corners = []
+			corner_dict_2d = {}
+			corner_dict_3d = {}
 			for j in range(len(corners)):
+				transformed_corners_2d = []
+				transformed_corners_3d = []
 				for i in range(corners[j].shape[1]):
 					pix = corners[j][0][i].astype(int)
 					pix_x = pix[0]
 					pix_y = pix[1]
 					pixels = k4a_float2_t((pix_x, pix_y))
-					c = device.calibration.convert_2d_to_2d(pixels, transformed_depth_image[pix_y, pix_x], K4A_CALIBRATION_TYPE_COLOR, K4A_CALIBRATION_TYPE_DEPTH)
-					transformed_corners.append(cv2.KeyPoint(c.xy.x, c.xy.y, 1))
+					c2 = device.calibration.convert_2d_to_2d(pixels, transformed_depth_image[pix_y, pix_x], K4A_CALIBRATION_TYPE_COLOR, K4A_CALIBRATION_TYPE_DEPTH)
+					transformed_corners_2d.append(cv2.KeyPoint(c2.xy.x, c2.xy.y, 1))
+
+					c3 = device.calibration.convert_2d_to_3d(pixels, transformed_depth_image[pix_y, pix_x], K4A_CALIBRATION_TYPE_COLOR, K4A_CALIBRATION_TYPE_DEPTH)
+					transformed_corners_3d.append(np.array([c3.xyz.x, c3.xyz.y, c3.xyz.z]))
+
+				corner_dict_2d[ids[j][0]] = transformed_corners_2d
+				corner_dict_3d[ids[j][0]] = transformed_corners_3d
+
 			cv2.aruco.drawDetectedMarkers(image, corners, ids)
-			cv2.drawKeypoints(color_image, transformed_corners, color_image, (0, 255, 0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+			
+			if 8 in corner_dict_2d:
+				cv2.drawKeypoints(color_image, corner_dict_2d[8], color_image, (0, 255, 0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+			if 9 in corner_dict_2d:
+				cv2.drawKeypoints(color_image, corner_dict_2d[9], color_image, (255, 0, 0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+			if 8 in corner_dict_3d and 9 in corner_dict_3d:
+				crn8 = corner_dict_3d[8]
+				c0 = crn8[0] + (crn8[1] - crn8[0]) * 0.5
+
+				# depth is 2.5 cm
+				crn9 = corner_dict_3d[9]
+				len1 = np.linalg.norm(crn9[1] - crn9[0])
+				c0 = c0 + (crn9[1] - crn9[0])/len1 * 30
+
+				# height is 8.5 cm
+				len2 = np.linalg.norm(crn8[0] - crn8[3])
+				c0 = c0 + (crn8[0] - crn8[3])/len2 * 85
+
+				pixel = k4a_float3_t((c0[0], c0[1], c0[2]))
+				c0_2d = device.calibration.convert_3d_to_2d(pixel, K4A_CALIBRATION_TYPE_DEPTH, K4A_CALIBRATION_TYPE_COLOR)
+
+				try:
+					image = cv2.circle(image, (int(c0_2d.xy.x), int(c0_2d.xy.y)), 10, (0, 255, 0), -1)
+				except:
+					print("NaN value in c0_2d")
+				
+			# plt.imshow(color_image, cmap='gray')
+			# plt.show()
+			# break
+
+			# if 8 in corner_dict_3d and 9 in corner_dict_3d:
+			# 	# cv2.imshow('Color Image',color_image)
+			# 	# cv2.imshow('ArUco Image',image)
+
+			# 	break
+
+#%%
 
 		# Get the colored depth
+		ret, depth_image = capture.get_colored_depth_image()
 
 		# Combine both images
 		# combined_image = cv2.addWeighted(color_image[:,:,:3], 0.7, depth_image, 0.3, 0)
@@ -70,10 +121,33 @@ if __name__ == "__main__":
 		# double the image size for better visibility
 
 		color_image = cv2.resize(color_image, (0,0), fx=1.7, fy=1.7)
-		# cv2.imshow('Depth Image',transformed_depth_image)
+		cv2.imshow('Depth Image',depth_image)
 		cv2.imshow('Color Image',color_image)
 		cv2.imshow('ArUco Image',image)
 
 		# Press q key to stop
 		if cv2.waitKey(1) == ord('q'):
 			break
+
+#%%
+# from pykinect_azure import K4A_CALIBRATION_TYPE_COLOR, K4A_CALIBRATION_TYPE_DEPTH, k4a_float2_t, k4a_float3_t
+
+# crn8 = corner_dict_3d[8]
+# c0 = crn8[0] + (crn8[1] - crn8[0]) * 0.5
+
+# # depth is 2.5 cm
+# crn9 = corner_dict_3d[9]
+# c0 = c0 + (crn9[1] - crn9[0])/np.linalg.norm(crn9[1] - crn9[0]) * 25
+
+# # height is 8.5 cm
+# c0 = c0 + (crn8[0] - crn8[3])/np.linalg.norm(crn8[0] - crn8[3]) * 85
+
+# # c0 = crn[0]
+
+# pixel = k4a_float3_t((c0[0], c0[1], c0[2]))
+# c0_2d = device.calibration.convert_3d_to_2d(pixel, K4A_CALIBRATION_TYPE_DEPTH, K4A_CALIBRATION_TYPE_COLOR)
+
+# new_image = image.copy()
+# new_image = cv2.cvtColor(new_image, cv2.COLOR_BGR2RGB)
+# new_image = cv2.circle(new_image, (int(c0_2d.xy.x), int(c0_2d.xy.y)), 15, (0, 255, 0), -1)
+# plt.imshow(new_image, cmap='gray')
