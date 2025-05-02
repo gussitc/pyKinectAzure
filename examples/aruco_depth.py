82419596b19f3d905a31f58bcf3d5d78dda9971d
Gustav <gtc@vitalthings.com>
2025-05-02 09:15:32 +0200

    major cleanup

--- examples/aruco_depth.py
+++ examples/aruco_depth.py
@@ -32,6 +32,132 @@ def get_aruco_box_frame(x_face, y_face, y_dist, z_dist, x_mirror=False, y_mirror
 
 	return c0, x_vec_norm, y_vec_norm, z_vec_norm
 
+def get_reference_frame(ids, corners):
+	if ids is not None:
+		corner_dict_2d = {}
+		corner_dict_3d = {}
+		for j in range(len(corners)):
+			transformed_corners_2d = []
+			transformed_corners_3d = []
+			for i in range(corners[j].shape[1]):
+				pix = corners[j][0][i].astype(int)
+				pix_x = pix[0]
+				pix_y = pix[1]
+				pixels = k4a_float2_t((pix_x, pix_y))
+				c2 = device.calibration.convert_2d_to_2d(pixels, transformed_depth_image[pix_y, pix_x], K4A_CALIBRATION_TYPE_COLOR, K4A_CALIBRATION_TYPE_DEPTH)
+				transformed_corners_2d.append(cv2.KeyPoint(c2.xy.x, c2.xy.y, 1))
+
+				c3 = device.calibration.convert_2d_to_3d(pixels, transformed_depth_image[pix_y, pix_x], K4A_CALIBRATION_TYPE_COLOR, K4A_CALIBRATION_TYPE_DEPTH)
+				transformed_corners_3d.append(np.array([c3.xyz.x, c3.xyz.y, c3.xyz.z]))
+
+			corner_dict_2d[ids[j][0]] = transformed_corners_2d
+			corner_dict_3d[ids[j][0]] = transformed_corners_3d
+
+		c0 = None
+		x_face = None
+		y_face = None
+		x_vec_norm = None
+		y_vec_norm = None
+		z_vec_norm = None
+		y_length = 150
+		y_offset = 25
+		z_dist = 85
+		if 8 in corner_dict_3d and 9 in corner_dict_3d:
+			y_dist = 0 - y_offset
+			x_face = corner_dict_3d[8]
+			y_face = corner_dict_3d[9]
+			y_mirror = False
+			x_mirror = False
+		elif 8 in corner_dict_3d and 11 in corner_dict_3d:
+			y_dist = 0 - y_offset
+			x_face = corner_dict_3d[8]
+			y_face = corner_dict_3d[11]
+			x_mirror = False
+			y_mirror = True
+		elif 10 in corner_dict_3d and 11 in corner_dict_3d:
+			y_dist = y_length - y_offset
+			x_face = corner_dict_3d[10]
+			y_face = corner_dict_3d[11]
+			x_mirror = True
+			y_mirror = True
+		elif 10 in corner_dict_3d and 9 in corner_dict_3d:
+			y_dist = y_length - y_offset
+			x_face = corner_dict_3d[10]
+			y_face = corner_dict_3d[9]
+			x_mirror = True
+			y_mirror = False
+
+		if x_face is not None and y_face is not None:
+			c0, x_vec_norm, y_vec_norm, z_vec_norm = get_aruco_box_frame(x_face, y_face, y_dist, z_dist, x_mirror, y_mirror)
+		
+		return c0, x_vec_norm, y_vec_norm, z_vec_norm, corner_dict_2d, corner_dict_3d
+	return None, None, None, None, None, None
+
+# def draw_aruco_box():
+
+# 	cv2.aruco.drawDetectedMarkers(image, corners, ids)
+	
+# 	if 8 in corner_dict_2d:
+# 		cv2.drawKeypoints(color_image, corner_dict_2d[8], color_image, (0, 255, 0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+# 	if 9 in corner_dict_2d:
+# 		cv2.drawKeypoints(color_image, corner_dict_2d[9], color_image, (255, 0, 0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+def draw_coordinate_frame(image, c0, x_vec_norm, y_vec_norm, z_vec_norm):
+	R = np.array([x_vec_norm, y_vec_norm, z_vec_norm]).T
+	try:
+		R_SVD = closest_rotation_matrix(R)
+		R = R_SVD
+	except:
+		print("SVD did not converge")
+
+	s = 150 # 20 cm
+
+	# coordinate frame in emblo coordinates
+	p0_e = np.array([s, 0, 0])
+	p1_e = np.array([0, s, 0])
+	p2_e = np.array([0, 0, s])
+
+	p0 = R @ p0_e + c0
+	p1 = R @ p1_e + c0
+	p2 = R @ p2_e + c0
+
+	p0_2d = device.calibration.convert_3d_to_2d(k4a_float3_t((p0[0], p0[1], p0[2])), K4A_CALIBRATION_TYPE_DEPTH, K4A_CALIBRATION_TYPE_COLOR)
+	p1_2d = device.calibration.convert_3d_to_2d(k4a_float3_t((p1[0], p1[1], p1[2])), K4A_CALIBRATION_TYPE_DEPTH, K4A_CALIBRATION_TYPE_COLOR)
+	p2_2d = device.calibration.convert_3d_to_2d(k4a_float3_t((p2[0], p2[1], p2[2])), K4A_CALIBRATION_TYPE_DEPTH, K4A_CALIBRATION_TYPE_COLOR)
+
+	pixel = k4a_float3_t((c0[0], c0[1], c0[2]))
+	c0_2d = device.calibration.convert_3d_to_2d(pixel, K4A_CALIBRATION_TYPE_DEPTH, K4A_CALIBRATION_TYPE_COLOR)
+
+	try:
+		image = cv2.circle(image, (int(c0_2d.xy.x), int(c0_2d.xy.y)), 10, (0, 255, 0), -1)
+
+		image = cv2.line(image, (int(p0_2d.xy.x), int(p0_2d.xy.y)), (int(c0_2d.xy.x), int(c0_2d.xy.y)), (0, 0, 255), 2)
+		image = cv2.line(image, (int(p1_2d.xy.x), int(p1_2d.xy.y)), (int(c0_2d.xy.x), int(c0_2d.xy.y)), (0, 255, 0), 2)
+		image = cv2.line(image, (int(p2_2d.xy.x), int(p2_2d.xy.y)), (int(c0_2d.xy.x), int(c0_2d.xy.y)), (255, 0, 0), 2)
+	except:
+		print("NaN value in c0_2d")
+	return image
+
+
+def aruco_detector(image):
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	image = cv2.cvtColor(image, cv2.COLOR_BGRA2BGR)
+	corners, ids, rejected = detector.detectMarkers(gray)
+	# ret, depth_image = capture.get_depth_image()
+
+	c0, x_vec_norm, y_vec_norm, z_vec_norm, corner_dict_2d, corner_dict_3d = get_reference_frame(ids, corners)
+
+	emblo_pos.append(c0)
+	x_vecs.append(x_vec_norm)
+	y_vecs.append(y_vec_norm)
+	z_vecs.append(z_vec_norm)
+
+	if c0 is not None:
+		image = draw_coordinate_frame(image, c0, x_vec_norm, y_vec_norm, z_vec_norm)
+	
+	return image
+
+
 if __name__ == "__main__":
 # if True:
 
@@ -94,122 +220,7 @@ if __name__ == "__main__":
 
 		_, image = capture.get_color_image()
 		image = image[:,:,:3]
-		gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-		image = cv2.cvtColor(image, cv2.COLOR_BGRA2BGR)
-		corners, ids, rejected = detector.detectMarkers(gray)
-		# ret, depth_image = capture.get_depth_image()
-		if ids is not None:
-			corner_dict_2d = {}
-			corner_dict_3d = {}
-			for j in range(len(corners)):
-				transformed_corners_2d = []
-				transformed_corners_3d = []
-				for i in range(corners[j].shape[1]):
-					pix = corners[j][0][i].astype(int)
-					pix_x = pix[0]
-					pix_y = pix[1]
-					pixels = k4a_float2_t((pix_x, pix_y))
-					c2 = device.calibration.convert_2d_to_2d(pixels, transformed_depth_image[pix_y, pix_x], K4A_CALIBRATION_TYPE_COLOR, K4A_CALIBRATION_TYPE_DEPTH)
-					transformed_corners_2d.append(cv2.KeyPoint(c2.xy.x, c2.xy.y, 1))
-
-					c3 = device.calibration.convert_2d_to_3d(pixels, transformed_depth_image[pix_y, pix_x], K4A_CALIBRATION_TYPE_COLOR, K4A_CALIBRATION_TYPE_DEPTH)
-					transformed_corners_3d.append(np.array([c3.xyz.x, c3.xyz.y, c3.xyz.z]))
-
-				corner_dict_2d[ids[j][0]] = transformed_corners_2d
-				corner_dict_3d[ids[j][0]] = transformed_corners_3d
-
-			cv2.aruco.drawDetectedMarkers(image, corners, ids)
-			
-			if 8 in corner_dict_2d:
-				cv2.drawKeypoints(color_image, corner_dict_2d[8], color_image, (0, 255, 0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-			if 9 in corner_dict_2d:
-				cv2.drawKeypoints(color_image, corner_dict_2d[9], color_image, (255, 0, 0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-			c0 = None
-			x_face = None
-			y_face = None
-			y_length = 150
-			y_offset = 25
-			z_dist = 85
-			if 8 in corner_dict_3d and 9 in corner_dict_3d:
-				y_dist = 0 - y_offset
-				x_face = corner_dict_3d[8]
-				y_face = corner_dict_3d[9]
-				y_mirror = False
-				x_mirror = False
-			elif 8 in corner_dict_3d and 11 in corner_dict_3d:
-				y_dist = 0 - y_offset
-				x_face = corner_dict_3d[8]
-				y_face = corner_dict_3d[11]
-				x_mirror = False
-				y_mirror = True
-			elif 10 in corner_dict_3d and 11 in corner_dict_3d:
-				y_dist = y_length - y_offset
-				x_face = corner_dict_3d[10]
-				y_face = corner_dict_3d[11]
-				x_mirror = True
-				y_mirror = True
-			elif 10 in corner_dict_3d and 9 in corner_dict_3d:
-				y_dist = y_length - y_offset
-				x_face = corner_dict_3d[10]
-				y_face = corner_dict_3d[9]
-				x_mirror = True
-				y_mirror = False
-
-			if x_face is not None and y_face is not None:
-				c0, x_vec_norm, y_vec_norm, z_vec_norm = get_aruco_box_frame(x_face, y_face, y_dist, z_dist, x_mirror, y_mirror)
-
-			if c0 is not None:
-				emblo_pos.append(c0)
-				x_vecs.append(x_vec_norm)
-				y_vecs.append(y_vec_norm)
-				z_vecs.append(z_vec_norm)
-
-				R = np.array([x_vec_norm, y_vec_norm, z_vec_norm]).T
-				try:
-					R_SVD = closest_rotation_matrix(R)
-					R = R_SVD
-				except:
-					print("SVD did not converge")
-
-				s = 150 # 20 cm
-
-				# coordinate frame in emblo coordinates
-				p0_e = np.array([s, 0, 0])
-				p1_e = np.array([0, s, 0])
-				p2_e = np.array([0, 0, s])
-
-				p0 = R @ p0_e + c0
-				p1 = R @ p1_e + c0
-				p2 = R @ p2_e + c0
-
-				p0_2d = device.calibration.convert_3d_to_2d(k4a_float3_t((p0[0], p0[1], p0[2])), K4A_CALIBRATION_TYPE_DEPTH, K4A_CALIBRATION_TYPE_COLOR)
-				p1_2d = device.calibration.convert_3d_to_2d(k4a_float3_t((p1[0], p1[1], p1[2])), K4A_CALIBRATION_TYPE_DEPTH, K4A_CALIBRATION_TYPE_COLOR)
-				p2_2d = device.calibration.convert_3d_to_2d(k4a_float3_t((p2[0], p2[1], p2[2])), K4A_CALIBRATION_TYPE_DEPTH, K4A_CALIBRATION_TYPE_COLOR)
-
-				pixel = k4a_float3_t((c0[0], c0[1], c0[2]))
-				c0_2d = device.calibration.convert_3d_to_2d(pixel, K4A_CALIBRATION_TYPE_DEPTH, K4A_CALIBRATION_TYPE_COLOR)
-
-				try:
-					image = cv2.circle(image, (int(c0_2d.xy.x), int(c0_2d.xy.y)), 10, (0, 255, 0), -1)
-
-					image = cv2.line(image, (int(p0_2d.xy.x), int(p0_2d.xy.y)), (int(c0_2d.xy.x), int(c0_2d.xy.y)), (0, 0, 255), 2)
-					image = cv2.line(image, (int(p1_2d.xy.x), int(p1_2d.xy.y)), (int(c0_2d.xy.x), int(c0_2d.xy.y)), (0, 255, 0), 2)
-					image = cv2.line(image, (int(p2_2d.xy.x), int(p2_2d.xy.y)), (int(c0_2d.xy.x), int(c0_2d.xy.y)), (255, 0, 0), 2)
-				except:
-					print("NaN value in c0_2d")
-				
-			# plt.imshow(color_image, cmap='gray')
-			# plt.show()
-			# break
-
-			# if 8 in corner_dict_3d and 9 in corner_dict_3d:
-			# 	# cv2.imshow('Color Image',color_image)
-			# 	# cv2.imshow('ArUco Image',image)
-
-			# 	break
-
-#%%
+		image = aruco_detector(image)
 
 		# Get the colored depth
 		ret, depth_image = capture.get_colored_depth_image()
