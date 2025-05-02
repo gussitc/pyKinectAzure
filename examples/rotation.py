48985f62891591eac1698187866c06dfb34c6e6b
Gustav <gtc@vitalthings.com>
2025-05-02 14:59:39 +0200

    minor rotation

--- examples/rotation.py
+++ examples/rotation.py
@@ -6,24 +6,27 @@ def closest_rotation_matrix(Q):
     R = U@VT
     return R
 
-data = np.load('kinect_calibration.npz')
+if __name__ == '__main__':
+    data = np.load('kinect_calibration.npz')
 
-x = data['x_vec']
-y = data['y_vec']
-z = data['z_vec']
+    x = data['x_vec']
+    y = data['y_vec']
+    z = data['z_vec']
 
-print("x: ", x)
-print("y: ", y)
-print("z: ", z)
+    print("x: ", x)
+    print("y: ", y)
+    print("z: ", z)
 
-R_approx = np.array([x, y, z]).T
-print("\nR_approx:")
-print(R_approx)
-print("\nIdentity:")
-print(R_approx @ R_approx.T)
+    R_approx = np.array([x, y, z]).T
+    print("\nR_approx:")
+    print(R_approx)
+    print("\nIdentity:")
+    print(R_approx @ R_approx.T)
 
-R = closest_rotation_matrix(R_approx)
-print("\nR:")
-print(R)
-print("\nIdentity:")
-print(R @ R.T)
+    R = closest_rotation_matrix(R_approx)
+    print("\nR:")
+    print(R)
+    print("\nIdentity:")
+    print(R @ R.T)
+
+    print(data['emblo_pos'])
