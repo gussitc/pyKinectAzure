44340360a07fec6b5b9e776b77319c5ccf2dbb8f
Gustav <gtc@vitalthings.com>
2025-05-07 09:38:36 +0200

    read and plot data from new json format

--- plot_pos.py
+++ plot_pos.py
@@ -1,9 +1,10 @@
-#%%
+# %%
 import numpy as np
 import matplotlib.pyplot as plt
 from rotation import closest_rotation_matrix
+import json
 
-#%%
+# %%
 def reject_outliers(data, m = 2.):
     d = np.abs(data - np.mean(data, axis=0))
     d_norm = np.linalg.norm(d, axis=1)
@@ -32,28 +33,83 @@ z_vec1 = reject_outliers(z_vec1)
 c0 = reject_outliers(c0)
 c1 = reject_outliers(c1)
 
-timestamps = np.loadtxt('timestamps.txt')
-last_timestamp = timestamps[-1]
+# %%
 
-#%%
+json_file0 = open('track_data_cam0.json', "r")
+json_file1 = open('track_data_cam1.json', "r")
+json_data0 = json.load(json_file0)
+json_data1 = json.load(json_file1)
+print(len(json_data0))
+print(len(json_data1))
 
-data = np.load('kinect_position.npz')
-cam0 = data['cam0']
-cam1 = data['cam1']
-cam0_confidence = data['cam0_confidence']
-cam1_confidence = data['cam1_confidence']
-timestamp0 = data['timestamp0']
-timestamp1 = data['timestamp1']
-is_track0 = data['is_track0']
-is_track1 = data['is_track1']
+# %%
 
-first_timestamp0 = last_timestamp // 1000 - (timestamp0[-1] - timestamp0[0])
-timestamp0 = first_timestamp0 + timestamp0
+def get_joint_positions(json_data, joint_id):
+    return np.array(
+        [
+            (
+                json_data[i]["joints"][joint_id]["position"]["v"]
+                if json_data[i]["num_bodies"] > 0
+                else [0, 0, 0]
+            )
+            for i in range(len(json_data))
+        ]
+    )
 
-first_timestamp1 = last_timestamp // 1000 - (timestamp1[-1] - timestamp1[0])
-timestamp1 = first_timestamp1 + timestamp1
+def get_joint_confidences(json_data, joint_id):
+    return np.array(
+        [
+            (
+                json_data[i]["joints"][joint_id]["confidence_level"]
+                if json_data[i]["num_bodies"] > 0
+                else 0
+            )
+            for i in range(len(json_data))
+        ]
+    )
 
-#%%
+def get_timestamps(json_data):
+    return np.array(
+        [
+            json_data[i]["utc_timestamp_ns"]
+            for i in range(len(json_data))
+        ]
+    )
+
+naval_joint_id = 1
+head_joint_id = 26
+joint_id = head_joint_id
+cam0 = get_joint_positions(json_data0, joint_id)
+cam1 = get_joint_positions(json_data1, joint_id)
+
+cam0_confidence = get_joint_confidences(json_data0, joint_id)
+cam1_confidence = get_joint_confidences(json_data1, joint_id)
+timestamp0 = get_timestamps(json_data0)
+timestamp1 = get_timestamps(json_data1)
+
+is_track0 = np.array([json_data0[i]["num_bodies"] > 0 for i in range(len(json_data0))])
+is_track1 = np.array([json_data1[i]["num_bodies"] > 0 for i in range(len(json_data1))])
+
+print(cam0.shape)
+print(cam1.shape)
+# %%
+
+# cam0 = data['cam0']
+# cam1 = data['cam1']
+# cam0_confidence = data['cam0_confidence']
+# cam1_confidence = data['cam1_confidence']
+# timestamp0 = data['timestamp0']
+# timestamp1 = data['timestamp1']
+# is_track0 = data['is_track0']
+# is_track1 = data['is_track1']
+
+# first_timestamp0 = last_timestamp // 1000 - (timestamp0[-1] - timestamp0[0])
+# timestamp0 = first_timestamp0 + timestamp0
+
+# first_timestamp1 = last_timestamp // 1000 - (timestamp1[-1] - timestamp1[0])
+# timestamp1 = first_timestamp1 + timestamp1
+
+# %%
 
 c0_mean = np.mean(c0, axis=0)
 c1_mean = np.mean(c1, axis=0)
@@ -67,7 +123,7 @@ y_vec1_mean = np.mean(y_vec1, axis=0)
 z_vec_mean = np.mean(z_vec, axis=0)
 z_vec1_mean = np.mean(z_vec1, axis=0)
 
-#%%
+# %%
 
 R = np.array([x_vec_mean, y_vec_mean, z_vec_mean]).T
 R_SVD = closest_rotation_matrix(R)
@@ -75,7 +131,7 @@ R_SVD = closest_rotation_matrix(R)
 R1 = np.array([x_vec1_mean, y_vec1_mean, z_vec1_mean]).T
 R1_SVD = closest_rotation_matrix(R1)
 
-#%%
+# %%
 
 # emblo coordinates seen by R10 sensor
 c0_room = np.array([[1.846738, 2.1159978, 1.4999355]]).T * 1000
@@ -89,11 +145,17 @@ cam1_room = cam1_room + c0_room
 cam0_room /= 1000
 cam1_room /= 1000
 
-#%%
+# %%
 cam0 = cam0_room.T
 cam1 = cam1_room.T
 
 #%%
+
+height_offset = 0.3
+cam0[:, 2] += height_offset
+cam1[:, 2] += height_offset
+
+# %%
 
 def get_closest_timestamp_index(timestamp, target_timestamp):
     # Find the index of the closest timestamp in the array
@@ -101,7 +163,7 @@ def get_closest_timestamp_index(timestamp, target_timestamp):
     return closest_index
 
 # if is_track0 or is_track1 is False, then replace with the data of the other camera
-for i in range(len(cam0)):
+for i in range(min(len(cam0), len(cam1))):
     if is_track0[i] == False and is_track1[i] == True:
         cam0[i] = cam1[get_closest_timestamp_index(timestamp1, timestamp0[i])]
     elif is_track1[i] == False and is_track0[i] == True:
@@ -111,31 +173,29 @@ for i in range(len(cam0)):
         pass
 
 
-#%%
-trajectory = np.loadtxt("trajectory_radar.txt")
-first_timestamp_ns = 1746444770635787611
-last_timestamp_ns = 1746444822292284255
-FPS = 17
+# %%
+radar_data = np.load("processed_radar_data.npz")
+trajectory = radar_data['trajectory']
+radar_timestamps = radar_data['timestamps']
 
-print((last_timestamp_ns - first_timestamp_ns) / 1e9)
-print(trajectory.shape[0] / FPS )
+radar_timestamps /= 1000
 
-radar_timestamps = np.arange(0, trajectory.shape[0]) * (1/FPS) * 1e6 + first_timestamp_ns // 1000
-print(radar_timestamps.shape)
-print(radar_timestamps[0])
+# TODO: why does this happen?
+radar_timestamps_offset = 500 * 1000
+radar_timestamps += radar_timestamps_offset
 
 start_idx = get_closest_timestamp_index(radar_timestamps, timestamp0[0])
 stop_idx = get_closest_timestamp_index(radar_timestamps, timestamp0[-1])
 trajectory = trajectory[start_idx:stop_idx]
 radar_timestamps = radar_timestamps[start_idx:stop_idx]
 
-#%%
+# %%
 radar_timestamps = (radar_timestamps - radar_timestamps[0]) / 1e6
 ref_timestamp = timestamp0[0]
 timestamp0 = (timestamp0 - ref_timestamp) / 1e6
 timestamp1 = (timestamp1 - ref_timestamp) / 1e6
 
-#%%
+# %%
 
 # plot x y and z in three subplots
 fig, axs = plt.subplots(4, 1, figsize=(10, 10))
@@ -186,8 +246,7 @@ plt.tight_layout()
 plt.show()
 
 
-
-#%%
+# %%
 import matplotlib.pyplot as plt
 
 tracks = np.loadtxt('trajectory_radar.txt')
@@ -204,4 +263,3 @@ plt.gca().set_aspect('equal', adjustable='box')
 plt.grid()
 plt.legend()
 plt.show()
-
