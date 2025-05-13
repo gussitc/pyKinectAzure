74c16a9b81af5ee095876c7e8a8346b3b5c1b37d
Gustav <gtc@vitalthings.com>
2025-05-13 12:50:55 +0200

    WIP simpler calibration

--- body_track_recorder.py
+++ body_track_recorder.py
@@ -5,6 +5,7 @@ import time
 import cv2
 import threading
 import json
+from aruco_detector import ArucoDetector
 from screeninfo import get_monitors
 
 monitor = get_monitors()[1]
@@ -15,8 +16,10 @@ window_size = 0
 # TODO: light model stopped working after merging with main branch
 use_lite_model = False
 upside_down = False
+calibration = False
 
 running = True
+
 
 def start_camera(device_info):
     device = device_info['device']
@@ -28,7 +31,7 @@ def close_devices(devices):
     for device_info in devices:
         device_info['device'].close()
 
-def process_camera(device_info, video_writer):
+def process_camera_tracking(device_info, video_writer):
     frame_count = 0
     total_frame_count = 0
     start_time = time.time()
@@ -106,8 +109,70 @@ def process_camera(device_info, video_writer):
     video_writer.release()
     device.close()
 
+
+def process_camera_calibration(device_info, aruco_detector: ArucoDetector):
+    global running
+
+    json_file_path = f"calibration_data_cam{device_info['index']}.json"
+    json_file = open(json_file_path, "w")
+    json_file.write("[")
+
+    window_name = f"Cam{device_info['index']}"
+    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+    cv2.moveWindow(window_name, window_size * (device_info["index"]), -50)
+    cv2.resizeWindow(window_name, window_size, window_size)
+
+    while running:
+        device = device_info['device']
+
+        capture = device.update()
+        ret_depth, depth_image = capture.get_transformed_depth_image()
+        if not ret_depth:
+            continue
+        
+        ret_color, color_image = capture.get_color_image()
+        color_image, c0, x_vec, y_vec, z_vec = aruco_detector.detect(device.calibration, color_image, depth_image)
+
+        calibration_data = {
+            "frame": total_frame_count,
+            "c0": c0,
+            "x_vec": x_vec}
+
+        json.dump(track_data, json_file)
+        json_file.write("\n,")
+
+        if upside_down:
+            combined_image = cv2.flip(combined_image, 0)
+
+        video_writer.write(combined_image)
+        window_name = f"Cam{device_info['index']}"
+        cv2.imshow(window_name, combined_image)
+
+        frame_count += 1
+        total_frame_count += 1
+        elapsed_time = time.time() - start_time
+        if elapsed_time > 1.0:
+            actual_fps = frame_count / elapsed_time
+            # print(f"Cam{device_info['index']} - Actual FPS: {actual_fps:.2f}")
+            frame_count = 0
+            start_time = time.time()
+
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            running = False
+            break
+
+    # remove the last newline and comma
+    json_file.seek(json_file.tell() - 2, 0)
+    json_file.truncate()
+    json_file.write("]")
+    json_file.close()
+
+    video_writer.release()
+    device.close()
+
 def main():
-    pykinect.initialize_libraries(track_body=True)
+    pykinect.initialize_libraries(track_body=not calibration)
 
     devices = []
     num_devices = pykinect.k4a_device_get_installed_count()
@@ -137,7 +202,10 @@ def main():
         device = pykinect.Device(i)
         device_config, device_type = device.device_configinit()
         device_config.depth_mode = k4a.K4A_DEPTH_MODE_WFOV_UNBINNED
-        device_config.color_resolution = k4a.K4A_COLOR_RESOLUTION_720P
+        if calibration:
+            device_config.color_resolution = k4a.K4A_COLOR_RESOLUTION_1080P
+        else:
+            device_config.color_resolution = k4a.K4A_COLOR_RESOLUTION_720P
         device_config.camera_fps = camera_fps
         bodyTracker = None
         devices.append({
@@ -180,18 +248,23 @@ def main():
     else:
         tracker_config.sensor_orientation = k4abt.K4ABT_SENSOR_ORIENTATION_DEFAULT
 
-    for i in range(num_devices):
-        devices[i]['bodyTracker'] = pykinect.start_body_tracker(calibration=devices[i]['device'].calibration, model_type=model)
-
     threads = []
+    aruco_detector = ArucoDetector()
+
     for i in range(num_devices):
-        video_writer = cv2.VideoWriter(
-            f'output_device_{i}.avi',
-            cv2.VideoWriter_fourcc(*'XVID'),
-            fps,
-            (frame_width, frame_height)
-        )
-        thread = threading.Thread(target=process_camera, args=(devices[i], video_writer))
+        if not calibration:
+            devices[i]['bodyTracker'] = pykinect.start_body_tracker(calibration=devices[i]['device'].calibration, model_type=model)
+
+            video_writer = cv2.VideoWriter(
+                f'output_device_{i}.avi',
+                cv2.VideoWriter_fourcc(*'XVID'),
+                fps,
+                (frame_width, frame_height)
+            )
+
+            thread = threading.Thread(target=process_camera_tracking, args=(devices[i], video_writer))
+        else:
+            thread = threading.Thread(target=process_camera_calibration, args=(devices[i], aruco_detector))
         threads.append(thread)
         thread.start()
 
