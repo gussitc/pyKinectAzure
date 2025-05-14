ebf3deb188761abc67b6a2e581bbfe6145ce9b4b
Gustav <gtc@vitalthings.com>
2025-05-14 12:12:46 +0200

    handle mew data format in plot pos script

--- plot_pos.py
+++ plot_pos.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from rotation import closest_rotation_matrix
 import json
+import glob
 
 # %%
 def reject_outliers(data, m=2.):
@@ -11,36 +12,41 @@ def reject_outliers(data, m=2.):
     outlier_idxs = np.where(d_norm > m * np.mean(d_norm))
     return np.delete(data, outlier_idxs, axis=0)
 
-# Load calibration data from JSON file
-with open('calibration.json', 'r') as f:
-    calibration_data = json.load(f)
+# Load calibration data for all cameras
+calibration_folder = 'data/calibration/cal_0001/'
+calibration_files = glob.glob(f"{calibration_folder}/calibration_data_cam*.json")
 
-# Extract calibration data for N cameras
 camera_calibrations = {}
-for entry in calibration_data:
-    camera_id = entry["camera_id"]
-    camera_calibrations[camera_id] = {
-        "c": np.array(entry["c"]),
-        "x_vec": np.array(entry["x_vec"]),
-        "y_vec": np.array(entry["y_vec"]),
-        "z_vec": np.array(entry["z_vec"]),
+for file in calibration_files:
+    with open(file, 'r') as f:
+        calibration_data = json.load(f)
+    cam_id = file.split('_cam')[-1].split('.json')[0]  # Extract camera ID from filename
+    camera_calibrations[cam_id] = {
+        "c0": np.array([calibration_data[i]["c0"] for i in range(len(calibration_data))]),
+        "x_vec": np.array([calibration_data[i]["x_vec"] for i in range(len(calibration_data))]),
+        "y_vec": np.array([calibration_data[i]["y_vec"] for i in range(len(calibration_data))]),
+        "z_vec": np.array([calibration_data[i]["z_vec"] for i in range(len(calibration_data))]),
     }
 
+#%%
 # Reject outliers for calibration data
 for cam_id, calib in camera_calibrations.items():
-    calib["c"] = reject_outliers(calib["c"])
+    calib["c0"] = reject_outliers(calib["c0"])
     calib["x_vec"] = reject_outliers(calib["x_vec"])
     calib["y_vec"] = reject_outliers(calib["y_vec"])
     calib["z_vec"] = reject_outliers(calib["z_vec"])
 
 # %%
-data_folder = 'short_data/'
-camera_data = {}
-camera_ids = list(camera_calibrations.keys())
+# Load tracking data for all cameras
+tracking_folder = 'data/tracking/track_0001/'
+tracking_files = glob.glob(f"{tracking_folder}/track_data_cam*.json")
 
-# Load JSON data for all cameras
-for cam_id in camera_ids:
-    with open(f"{data_folder}track_data_cam{cam_id}.json", "r") as f:
+camera_data = {}
+camera_ids = [file.split('_cam')[-1].split('.json')[0] for file in tracking_files]  # Extract camera IDs
+
+for file in tracking_files:
+    with open(file, 'r') as f:
+        cam_id = file.split('_cam')[-1].split('.json')[0]
         camera_data[cam_id] = json.load(f)
 
 # Helper functions
@@ -95,7 +101,7 @@ camera_means = {}
 camera_rotations = {}
 
 for cam_id, calib in camera_calibrations.items():
-    c_mean = np.mean(calib["c"], axis=0)
+    c_mean = np.mean(calib["c0"], axis=0)
     x_vec_mean = np.mean(calib["x_vec"], axis=0)
     y_vec_mean = np.mean(calib["y_vec"], axis=0)
     z_vec_mean = np.mean(calib["z_vec"], axis=0)
@@ -122,24 +128,26 @@ def get_closest_timestamp_index(timestamp, target_timestamp):
     closest_index = (np.abs(timestamp - target_timestamp)).argmin()
     return closest_index
 
-for i in range(len(camera_ids)):
-    cam_id = camera_ids[i]
-    for j in range(len(camera_positions[cam_id])):
-        if not camera_tracks[cam_id][j]:
-            for other_cam_id in camera_ids:
-                if other_cam_id != cam_id and camera_tracks[other_cam_id][j]:
-                    closest_idx = get_closest_timestamp_index(
-                        camera_timestamps[other_cam_id], camera_timestamps[cam_id][j]
-                    )
-                    camera_positions[cam_id][j] = camera_positions[other_cam_id][closest_idx]
-                    break
+# TODO: Implement the logic to fill in missing camera positions
+# for i in range(len(camera_ids)):
+#     cam_id = camera_ids[i]
+#     for j in range(len(camera_positions[cam_id])):
+#         if not camera_tracks[cam_id][j]:
+#             for other_cam_id in camera_ids:
+#                 if other_cam_id != cam_id and camera_tracks[other_cam_id][j]:
+#                     closest_idx = get_closest_timestamp_index(
+#                         camera_timestamps[other_cam_id], camera_timestamps[cam_id][j]
+#                     )
+#                     camera_positions[cam_id][j] = camera_positions[other_cam_id][closest_idx]
+#                     break
 
 # %%
 # Plot data for all cameras
 fig, axs = plt.subplots(4, 1, figsize=(10, 10))
 
+reference_timestamp = camera_timestamps[camera_ids[0]][0]
 for cam_id in camera_ids:
-    timestamps = (camera_timestamps[cam_id] - camera_timestamps[cam_id][0]) / 1e6
+    timestamps = (camera_timestamps[cam_id] - reference_timestamp) / 1e6
     positions = camera_room_positions[cam_id]
     axs[0].plot(timestamps, positions[:, 0], label=f'cam{cam_id} x')
     axs[1].plot(timestamps, positions[:, 1], label=f'cam{cam_id} y')
